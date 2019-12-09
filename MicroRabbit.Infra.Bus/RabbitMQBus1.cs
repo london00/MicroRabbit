2300bcaeed3024151689351da0424e7136507269
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-12-09 11:41:28 -0600

    Create publish method in Infra Project

---
+++ MicroRabbit.Infra.Bus/RabbitMQBus1.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using MicroRabbit.Doman.Core.Bus;
+using MicroRabbit.Doman.Core.Commands;
+using MicroRabbit.Doman.Core.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Infra.Bus
+{
+    public sealed class RabbitMQBus : IEventBus
+    {
+        private readonly IMediator _mediator;
+        private readonly Dictionary<string, List<Type>> _handlers;
+        private readonly List<Type> _eventTypes;
+
+        public RabbitMQBus(IMediator mediator)
+        {
+            _mediator = mediator;
+            _handlers = new Dictionary<string, List<Type>>();
+            _eventTypes = new List<Type>();
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+
+        public void Publish<T>(T @event) where T : Event
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var eventName = @event.GetType().Name;
+
+                channel.QueueDeclare(eventName, false, false, false, null);
+
+                var message = JsonConvert.SerializeObject(@event);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", eventName, null, body);
+            }
+        }
+
+        public void Suscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
