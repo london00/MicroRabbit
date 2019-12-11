89a2d855b9f651db91248bf37448aad80d861853
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-12-11 15:25:45 -0600

    Add DB Context

---
+++ MicroRabbit.Banking.Data/Context/Infrastructure/BankingDBContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class BankingDBContextFactory : IDesignTimeDbContextFactory<BankingDBContext>
+    {
+        public BankingDBContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BankingDBContext>();
+            optionsBuilder.UseSqlServer("Server=DESKTOP-5GIEO5J\\SQLEXPRESS;Database=BankingDB;Trusted_Connection=True;MultipleResultsSets=True");
+
+            return new BankingDBContext(optionsBuilder.Options);
+        }
+    }
+}
