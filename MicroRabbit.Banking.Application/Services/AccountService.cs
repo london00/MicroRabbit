be139e994cf638e923d22ebab94cc5ca2da8737e
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-12-18 10:21:23 -0600

    Banking Microservice

--- MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -1,6 +1,9 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Doman.Core.Bus;
 using System.Collections.Generic;
 
 namespace MicroRabbit.Banking.Application.Services
@@ -8,14 +11,27 @@ namespace MicroRabbit.Banking.Application.Services
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly IEventBus _bus;
 
-        public AccountService(IAccountRepository accountRepository)
+        public AccountService(IAccountRepository accountRepository, IEventBus bus)
         {
             this._accountRepository = accountRepository;
+            this._bus = bus;
         }
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accooutTransfer)
+        {
+            var createTransfer = new CreateTransferCommand(
+                accooutTransfer.FromAccount,
+                accooutTransfer.ToAccount,
+                accooutTransfer.TransferAmount
+                );
+
+            _bus.SendCommand(createTransfer);
         }
     }
 }
---
+++ MicroRabbit.Banking.Domain/Commands/TransferCommand.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabbit.Doman.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Domain.Commands
+{
+    public abstract class TransferCommand : Command
+    {
+        public int From { get; protected set; }
+        public int To { get; protected set; }
+        public decimal Ammount { get; protected set; }
+    }
+}
