68608fc445dd4eb8b26b300a8abc0288a2bc0606
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-12-20 11:56:39 -0600

    Create Transfer Repository

---
+++ MicroRabbit.Transfer.Application/Services/TransferService.cs
@@ -0,0 +1,26 @@
+﻿using MicroRabbit.Doman.Core.Bus;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Transfer.Application.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly ITransferRepository _transferRepository;
+        private readonly IEventBus _bus;
+
+        public TransferService(ITransferRepository transferRepository, IEventBus bus)
+        {
+            this._bus = bus;
+            this._transferRepository = transferRepository;
+        }
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return this._transferRepository.GetTransferLogs();
+        }
+    }
+}
---
+++ MicroRabbit.Transfer.Data/Repository/TransferRepository.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Transfer.Data.Repository
+{
+    public class TransferRepository : ITransferRepository
+    {
+        private TransferDBContext _ctx;
+
+        public TransferRepository(TransferDBContext ctx)
+        {
+            this._ctx = ctx;
+        }
+
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return _ctx.TransferLogs;
+        }
+    }
+}
